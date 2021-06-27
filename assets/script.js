3fde6238b7cc06b82711a8ff44a3cd658658e24b
tiz314 <tizianoradicchi14@gmail.com>
2021-06-27 21:08:54 +0200

    work

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start);
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+}
+
+const obj = document.getElementById("value");
+satLaunched = 1314;
+newOrders = 4502;
+
+
+//animateValue(obj, 0, satLaunched, 4000);
+  
